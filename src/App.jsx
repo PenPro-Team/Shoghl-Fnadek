1a2592867969c80f0a805ecb108bcd1cea3ff91b
AbdAlla-AboElMagd <abd.moh1990@gmail.com>
2025-03-30 16:51:36 +0200

    Make The Products Page and Product Details Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 // import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Aboutus from "./Pages/aboutus"
-import Contactus from "./Pages/contactus"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Aboutus from "./Pages/aboutus";
+import Contactus from "./Pages/contactus";
 import Register from "./Pages/register";
 import Login from "./Pages/login";
+import Products from "./Pages/products";
+import ProductDetails from "./Pages/productDetails";
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-      <Router>
-        <Routes>
-              <Route path="/aboutus" element={<Aboutus />} />
-              <Route path="/contactus" element={<Contactus />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
-  )
+    <Router>
+      <Routes>
+        <Route path="/aboutus" element={<Aboutus />} />
+        <Route path="/contactus" element={<Contactus />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
