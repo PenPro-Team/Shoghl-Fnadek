7f11e3f20d577d903b084fddfe453d3ec0d1c57b
Teleb0xCyMage <163046737+jackiee211@users.noreply.github.com>
2025-04-03 12:06:40 +0200

    added cart, checkout, payment Via Vodafone Cash pages

--- src/Network/Remote/AxiosInstance.jsx
+++ src/Network/Remote/AxiosInstance.jsx
@@ -5,3 +5,7 @@ let url = "http://127.0.0.1:8000";
 export const AxiosProductsInstance = axios.create({
   baseURL: `${url}/products/`,
 });
+
+export const AxiosOrdersInstance = axios.create({
+  baseURL: `${url}/api/orders`,  // Updated to match new API endpoint
+});
