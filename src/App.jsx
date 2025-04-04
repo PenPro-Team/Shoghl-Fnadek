64991072456817c9d5fbb4537cdcaebc89bf2317
Mo2024-cloud <mrfreelancer.org@gmail.com>
2025-04-04 03:19:06 +0200

    Make Footer Appear In all Pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Navbar from "./components/navbar";
 import { CartProvider } from "./context/CartContext";
 import Checkout from "./Pages/checkout";
 import PaymentConfirmation from "./Pages/paymentConfirmation";
+import Footer from "./Pages/Footer";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/payment-confirmation" element={<PaymentConfirmation />} />
         </Routes>
+        <Footer />
       </Router>
     </CartProvider>
   );
